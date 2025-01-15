fe707b216adbee1980c1df87c4333a1fb49b7d5f
Nauval Zulfikar <70472247+NauvalZulfikar@users.noreply.github.com>
2025-01-15 22:32:17 +0000

    product_catalogue.py

--- product_catalogue.py
+++ product_catalogue.py
@@ -12,10 +12,6 @@ def product_catalogue():
         
     st.write(dfpc.sort_values(by=['Start Time','End Time']))
     
-    # Editable DataFrame
-    st.subheader("Similarity")
-    edited_df = st.data_editor(similarity_df, num_rows="dynamic", use_container_width=True)
-
     # Update the source DataFrame in session state
     st.session_state.similarity_df = edited_df
 
