16560f556ae0722c636b0f3fcbc91c24de269a8b
Nauval Zulfikar <70472247+NauvalZulfikar@users.noreply.github.com>
2025-01-15 13:39:24 +0000

    product_catalogue.py

--- product_catalogue.py
+++ product_catalogue.py
@@ -3,4 +3,9 @@ import streamlit as st
 from scheduler import dfm
 
 def product_catalogue():
+    dfm_list = ['Order Processing Date','Promised Delivery Date','Start Time','End Time']
+    
+    for i in dfm_list:
+        dfm[i] = dfm[i].dt.strftime('%Y-%m-%d %H:%M')
+        
     st.write(dfm.sort_values(by=['Start Time','End Time']))
