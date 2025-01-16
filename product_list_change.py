674f88feca3c78c3e7cdf1f6b3337afb7201cc1d
Nauval Zulfikar <70472247+NauvalZulfikar@users.noreply.github.com>
2025-01-16 20:47:24 +0000

    product_list_change.py

--- product_list_change.py
+++ product_list_change.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+from scheduler import dfm 
 
 def product_list_change():
     st.title("Product List Management")
@@ -17,34 +18,49 @@ def product_list_change():
     )
 
     if selected_visualization == "Add Product":
-        st.subheader("Add New Product")
         input1 = st.text_input("Sr. No:")
         input2 = st.text_input("Product Name:")
         input3 = st.date_input("Order Processing Date:")
         input4 = st.date_input("Promised Delivery Date:")
         input5 = st.text_input("Quantity Required:")
         input6 = st.text_input("Components:")
-        input1 = st.text_input("Operation:")
-        input2 = st.text_input("Process Type:")
-        input3 = st.text_input("Machine Number:")
-        input4 = st.text_input("Run Time (min/1000):")
-        input5 = st.text_input("Cycle Time:")
-        input6 = st.text_input("Setup Time (seconds):")
+        input7 = st.text_input("Operation:")
+        input8 = st.text_input("Process Type:")
+        input9 = st.text_input("Machine Number:")
+        input10 = st.text_input("Run Time (min/1000):")
+        input11 = st.text_input("Cycle Time:")
+        input12 = st.text_input("Setup Time (seconds):")
 
-        if st.button("Add Product"):
+        if st.button("Submit"):
+            new_row = pd.DataFrame({
+                'Sr. No':input1,
+                'Product Name':input2,
+                'Order Processing Date':input3,
+                'Promised Delivery Date':input4,
+                'Quantity Required':input5,
+                'Components':input6,
+                'Operation':input7,
+                'Process Type':input8,
+                'Machine Number':input9,
+                'Run Time (min/1000)':input10,
+                'Cycle Time':input11,
+                'Setup Time (seconds)':input12,
+            })
+            dfm = pd.concat([dfm,pd.DataFrame(new_row)])        
             st.success(f"Product '{input2}' added successfully.")
 
     elif selected_visualization == "Delete Product":
-        st.subheader("Delete Product")
-        input1 = st.text_input("UniqueID:")
+        input1 = st.number_input("UniqueID:")
 
-        if st.button("Delete Product"):
+        if st.button("Delete"):
+            dfm = dfm[dfm['UniqueID']!=input1]
             st.warning(f"Product with ID '{input1}' deleted successfully.")
 
     elif selected_visualization == "Swap Product":
-        st.subheader("Swap Product")
-        input1 = st.text_input("First UniqueID:")
-        input2 = st.text_input("SecondID:")
+        input1 = st.number_input("First UniqueID:")
+        input2 = st.number_input("Second UniqueID:")
 
-        if st.button("Swap Products"):
+        if st.button("Swap"):
+            dfm.loc[df['UniqueID']==input1,'UniqueID'] = input2
+            dfm.loc[df['UniqueID']==input2,'UniqueID'] = input1
             st.info(f"Product '{input1}' swapped with product '{input2}' successfully.")
