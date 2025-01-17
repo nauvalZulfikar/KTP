d8008a5d1ba1ce8e1ec2a168a050417115797491
brndwrlsqnchu7vrsaakpr <70472247+NauvalZulfikar@users.noreply.github.com>
2025-01-17 11:49:42 +0000

    visualisation.py

--- visualisation.py
+++ visualisation.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from streamlit_autorefresh import st_autorefresh
 import streamlit as st
-from scheduler import adjust_to_working_hours_and_days, calculate_machine_utilization
+# from scheduler import adjust_to_working_hours_and_days, calculate_machine_utilization
 
 # Create Bar Charts
 def create_bar_chart(data, x_col, y_col, color=None):
