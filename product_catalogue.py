4681842ce6e2ebca44a8e5ac447eceea98defce7
brndwrlsqnchu7vrsaakpr <70472247+NauvalZulfikar@users.noreply.github.com>
2025-01-18 17:19:41 +0000

    product_catalogue.py

--- product_catalogue.py
+++ product_catalogue.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import streamlit as st
 import datetime as dt
-from scheduler import calculate_machine_utilization, initialise_state
+from scheduler import calculate_machine_utilization#, initialise_state
 
-initialise_state()
+# initialise_state()
 
 st.write(st.session_state)
 def product_catalogue():
